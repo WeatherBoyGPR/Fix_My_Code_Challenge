587176a65e720fbc46e9142c2398a411c2ad86b2
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-06-03 20:10:56 +0000

    updated square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -8,7 +8,7 @@ class Square():
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """ init """
+        """ init method, sets height and width """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
